5e2e1ae34754d2b04bd1ecd13aa8ad247bf53bc9
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-06-14 15:53:00 +0300

    components_apartments-lict

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,66 @@
 <template>
-<h1>{{title}}</h1>
+  <h1>{{ title }}</h1>
   <ButtonStandard @click="incremen">Click me</ButtonStandard>
-
+  <!-- <StarRating :rating="2.5" /> -->
+    <DirectiveesTest />
+    <ApartmentsList :items="apartments"/>
 </template>
 
 <script>
-
 import ButtonStandard from "./components/ButtonStandard.vue";
+import DirectiveesTest from "./components/DirectiveesTest.vue";
+// import StarRating from "./components/StarRating.vue";
+import ApartmentsList from "./components/apartment/ApartmentsList.vue";
+import apartments from "./components/apartment/apartments";
 
 export default {
   name: "App",
   components: {
     ButtonStandard,
-
+    DirectiveesTest,
+    // StarRating,
+    ApartmentsList,
+    
   },
-  data(){
-    return{
-        amountOfClicks: 0
-    }
+  data() {
+    return {
+      amountOfClicks: 0,
+      apartments,
+      apartment: {
+        id: "42342",
+        title:
+          "Aut qui adipisgi distinctio maiores molestiae sit est inventjre vero",
+        descr:
+          "Non perferendis rerum a in nisi exercitationem dolorum perferendis",
+        price: 2032,
+        rating: 4.7,
+        location: {
+          city: "Kherson",
+        },
+        owner: {
+          name: "Ellen",
+          phone: "1324345",
+          email: "Ellen@gmail.com",
+        },
+      },
+    };
+  },
+  computed: {
+    title() {
+      return `Amount ot clicks ${this.amountOfClicks}`;
     },
-    computed :{
-      title(){
-        return `Amount ot clicks ${this.amountOfClicks}`
-      }
-},
-methods:{
-  incremen(){
-    this.amountOfClicks += 1
-  }
-}}
+  },
+  methods: {
+    incremen() {
+      this.amountOfClicks += 1;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/apartment/ApartmentsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="apartment-list">
+    <ApartmentItem
+      v-for="{ id, descr, price, rating, imgUrl } in items"
+      :key="id"
+      :descr="descr"
+      :price="price"
+      :rating="rating"
+      :imgSrc="imgUrl"
+      class="apartment-list__item"
+    />
+  </div>
+</template>
+
+<script>
+import ApartmentItem from "./ApartmentItem.vue";
+
+export default {
+  name: "ApartmentsList",
+  components: {
+    ApartmentItem,
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.apartment-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -15px;
+  margin-right: -15px;
+  &__item {
+    margin-bottom: 30px;
+  }
+}
+</style>
